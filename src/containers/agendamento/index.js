1052031b1889d3d7dd841c2465940789593ac4a7
Larissa Mourullo <larismourullo@live.com>
2018-07-16 02:10:01 -0300

    :sparkles: Create new routes and containers

---
+++ src/containers/agendamento/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Agendamento extends Component {
+  render() {
+    return <p>Página de Agendamento</p>;
+  }
+}
--- src/containers/app.js
+++ src/containers/app.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Home from './home';
+import Agendamento from './agendamento';
+import Login from './login';
 
 export default class App extends Component {
   render() {
-    return <Route exact path="/" component={Home} />;
+    return (
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/agendamento" component={Agendamento} />
+        <Route path="/login" component={Login} />
+      </Switch>
+    );
   }
 }
