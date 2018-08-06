31656780b1066b4423dd91cfb2dd209ebabec75a
Larissa Mourullo <larismourullo@live.com>
2018-08-05 22:12:54 -0300

    :camera_flash: Updating snapshots

--- src/components/button/button.spec.js
+++ src/components/button/button.spec.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Button from './index';
+import { mount } from 'enzyme';
 
+const rendered = mount(<Button>Button Component</Button>);
 const snapshot = renderer.create(<Button>Button Component</Button>).toJSON();
 
-it('Should Title match to snapshot', () => {
+it('Should Button tag return output correctly', () => {
+  expect(rendered.text()).toBe('Button Component');
+});
+
+it('Should Button match to snapshot', () => {
   expect(snapshot).toMatchSnapshot();
 });
