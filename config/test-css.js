c788070b1452e46ac69430f3000fd198fc18a2e4
Larissa Mourullo <larismourullo@live.com>
2018-08-26 22:42:48 -0300

    :rocket: Deploy

--- config/test-css.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  verbose: true,
-  projects: [{
-    runner: 'jest-runner',
-    testURL: 'http://localhost/',
-  }]
-};
--- src/components/card/index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import { Card, CardText } from 'reactstrap';
-import './style.css';
-
-export default ({ children, decorative, ...props }) => (
-  <Card {...props}>
-    <CardText>{children}</CardText>
-  </Card>
-);
