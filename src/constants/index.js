efc6983cf6720239b71796be74177d37fe90e22f
heriktedesco <herik.tedesco@al.infnet.edu.br>
2018-09-21 00:11:20 -0300

    add base url

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Constants {
 	constructor() {
 		super();
-		global.daoUrlBaseSchedules = '';
-		global.daoUrlBaseCitizens = '';
+		global.daoUrlBaseSchedules = 'http://localhost:8080/v1/scheduling';
+		global.daoUrlBaseCitizens = 'http://localhost:8080/v1/citezen';
+		global.daoUrlBaseAddresses = 'http://localhost:8080/v1/address';
+		global.daoUrlBaseAuth = 'http://localhost:8080/v1/authentication';
 	}
 }
